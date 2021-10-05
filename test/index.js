2dd0eee7593178a2d22641d50151b27e779aab55
Adorabus <adorabusfox@gmail.com>
2021-10-05 01:13:21 -0700

    Prefer release versions

--- test/index.js
+++ test/index.js
@@ -4,27 +4,26 @@ const fs = require('fs')
 const DIR = 'test_dir'
 
 if (!fs.existsSync(DIR)) {
-    fs.mkdirSync(DIR)
+  fs.mkdirSync(DIR)
 }
 
 async function test () {
-    const updater = new ForgeUpdater(DIR)
-    await updater.init('1.16.4')
+  const updater = new ForgeUpdater(DIR)
+  await updater.init('1.16.4')
 
-    updater.addModByID(240633)
+  updater.addModByID(240633)
 
-    try {
-        const checkRes = await updater.check()
-        updater.update(checkRes.updates)
-            .on('done', (err) => {
-                if (err) console.log(err)
-                console.log('Done')
-            })
-        console.log(checkRes)
-    } catch (error) {
-        console.log('Test failed.', error.message)
-    }
-
+  try {
+    const checkRes = await updater.check()
+    updater.update(checkRes.updates)
+      .on('done', (err) => {
+        if (err) console.log(err)
+        console.log('Done')
+      })
+    console.log(checkRes)
+  } catch (error) {
+    console.log('Test failed.', error.message)
+  }
 }
 
 test().then(console.log).catch(console.error)
